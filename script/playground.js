21d34738f20a8e5f36aabae84b8a2f84facfbf42
ntalcus <alcus.noah@gmail.com>
2019-12-12 20:10:02 -0800

    working on the typewriter stuff (mostly there except the blinking cursor doesn't work

--- script/playground.js
+++ script/playground.js
@@ -1,21 +1,26 @@
 var i = 0;
 var txt = 'Noah Theodore Alcus'; /* The text */
-var speed = 50; /* The speed/duration of the effect in milliseconds */
+var speed = 75; /* The speed/duration of the effect in milliseconds */
 
-function typeWriter() {
+async function typeWriter() {
     let forward = true;
+    let j = 0;
     while (true) {
-        if (forward) {
+        if (forward && i < txt.length) {
             document.getElementById("type_writer").innerHTML += txt.charAt(i);
             i++;
-            sleep(speed);
-            // continue;
-        } else if (!forward && i > 0) {
+            await sleep(speed);
+            continue;
+        } else if (!forward && i >= 0) {
             document.getElementById("type_writer").innerHTML = txt.slice(0,i);
             i--;
-            sleep(speed);
+            await sleep(speed);
             continue;
-        }
+        } else if (!forward) {
+	    await sleep(speed * 75);
+	} else if (forward) {
+	    await sleep(speed * 15);
+	}
         forward = !forward;
     }
 }
@@ -25,4 +30,3 @@ const sleep = (milliseconds) => {
     return new Promise(resolve => setTimeout(resolve, milliseconds))
 }
 
-window.onload= typeWriter();
